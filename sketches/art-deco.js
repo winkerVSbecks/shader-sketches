393c9d648704d382075055ea575c20028cf2710b
winkerVSbecks <varunvachhar@gmail.com>
2023-01-21 14:38:50 -0500

    art deco

---
+++ sketches/art-deco.js
@@ -0,0 +1,94 @@
+const canvasSketch = require('canvas-sketch');
+const createShader = require('canvas-sketch-util/shader');
+const glsl = require('glslify');
+const Random = require('canvas-sketch-util/random');
+const tome = require('chromotome');
+const THREE = require('three');
+const Color = require('canvas-sketch-util/color');
+
+// Setup our sketch
+const settings = {
+  dimensions: [1080, 1080],
+  context: 'webgl',
+  animate: true,
+  duration: 2,
+};
+
+// Your glsl code
+const frag = glsl(/* glsl */ `
+  precision highp float;
+
+  uniform float time;
+  varying vec2 vUv;
+  uniform vec3 background;
+  uniform vec3 foreground;
+
+  #define PI 3.14159265359
+
+  vec2 rotate2D(vec2 _st, float _angle){
+    _st -= 0.5;
+    _st =  mat2(cos(_angle),-sin(_angle),
+                sin(_angle),cos(_angle)) * _st;
+    _st += 0.5;
+    return _st;
+  }
+
+  void main () {
+    vec2 st = rotate2D(vUv, PI * 0.25);
+
+    st *= 5.0;
+    if (mod(st.x, 2.) > 1.) { st.y += .25; }
+    st = fract(st);
+
+    vec3 color = vec3(1.0);
+
+    for(int i = -2; i < 9; i++) {
+		  float t = 1. * float(i) / 8. + time/8.;
+    	vec2 bl = step(vec2(0.02), st - vec2(t));
+	    float pct = bl.x * bl.y;
+	    color -= st.x < t || st.y < t  ? vec3(0.) : 1. - vec3(pct * vec3(1.0));
+    }
+
+    color = vec3(1.0) - color;
+    color = mix(color, background, foreground);
+
+    gl_FragColor = vec4(color, 1.0);
+  }
+`);
+
+const sketch = ({ gl }) => {
+  const { background, foreground } = colors();
+
+  return createShader({
+    gl,
+    frag,
+    uniforms: {
+      time: ({ time }) => time,
+      background,
+      foreground,
+    },
+  });
+};
+
+function colors(minContrast = 1) {
+  let palette = tome.get();
+  if (!palette.background) palette = tome.get();
+  console.log(palette.name);
+
+  const background = palette.background;
+
+  const colors = palette.colors.filter(
+    (color) =>
+      Color.contrastRatio(background, color) >= minContrast &&
+      color !== background
+  );
+
+  const foreground = Random.pick(colors);
+
+  return {
+    background: new THREE.Color(background).toArray(),
+    foreground: new THREE.Color(foreground).toArray(),
+  };
+}
+
+canvasSketch(sketch, settings);
