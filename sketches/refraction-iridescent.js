4e79eb5acd34db080599985b8ca0a54209cd0dfa
winkerVSbecks <varunvachhar@gmail.com>
2023-05-08 22:35:49 -0400

    refraction

--- sketches/refraction-iridescent.js
+++ sketches/refraction-iridescent.js
@@ -21,8 +21,12 @@ const frag = glsl(/*glsl*/ `
   uniform float playhead;
   uniform vec2 resolution;
   uniform vec3 cameraPos;
+  uniform vec3 lightPos;
   uniform float lensLength;
   uniform bool rotateCamera;
+  uniform bool addSpecular;
+  uniform float mixBaseAndIridescent;
+  uniform vec3 tint;
   uniform vec2 mouse;
   uniform samplerCube envMap;
 
@@ -40,6 +44,7 @@ const frag = glsl(/*glsl*/ `
   #pragma glslify: normal = require('glsl-sdf-normal', map = doModel)
   #pragma glslify: camera = require('glsl-camera-ray')
   #pragma glslify: square   = require('glsl-square-frame')
+  #pragma glslify: blinnPhongSpec = require('glsl-specular-blinn-phong')
   #pragma glslify: rotate = require('glsl-rotate/rotate')
 
   // Using some magic folding stuff described here: https://youtu.be/0RWaR7zApEo?t=181
@@ -101,13 +106,9 @@ const frag = glsl(/*glsl*/ `
     vec3 bg = vec3(0.);
     // Bootstrap a raytracing scene
     float cameraAngle  = rotateCamera ? 2. * PI * playhead : 0.0;
-    vec3  rayOrigin    = cameraPos;// * vec3(sin(cameraAngle), 1.0, cos(cameraAngle));
-    if (rotateCamera) {
-      rayOrigin.xz = rotate(rayOrigin.xz, cameraAngle);
-    } else {
-      rayOrigin.yz = rotate(rayOrigin.yz, -mouse.y * PI + 1.);
-      rayOrigin.xz = rotate(rayOrigin.xz, -mouse.x * PI * 2.);
-    }
+    vec3  rayOrigin    = cameraPos; // * vec3(sin(cameraAngle), 1.0, cos(cameraAngle));
+    rayOrigin.yz = rotate(rayOrigin.yz, -mouse.y * PI + 1.);
+    rayOrigin.xz = rotate(rayOrigin.xz, -mouse.x * PI * 2.);
     vec3  rayTarget    = vec3(0, 0, 0);
     vec2  screenPos    = square(resolution.xy);
     vec3  rayDirection = camera(rayOrigin, rayTarget, screenPos, lensLength);
@@ -164,11 +165,41 @@ const frag = glsl(/*glsl*/ `
       float dens = .1;
       float optDist = exp(-collisionIn.x * dens);
 
-      refractedTex = refractedTex * optDist;
+      refractedTex = refractedTex*optDist; //*vec3(1., .05,.2);
 
       float fresnel = pow(1. + dot(rayDirection, nor), 5.);
 
+      // From Thomas Hooper's https://www.shadertoy.com/view/llcXWM
+      vec3 eyeDirection = normalize(rayOrigin - pos);
+      vec3 lightDirection = normalize(lightPos - pos);
+
+      // basic blinn phong lighting
+      float power = blinnPhongSpec(lightDirection, eyeDirection, nor, 0.5);
+      color = vec3(power, power, power) * tint;
+
       color = mix(refractedTex, reflectionOutside, fresnel);
+
+      // // iridescent lighting
+      // vec3 reflection = reflect(rayDirection, nor);
+      // vec3 dome = vec3(0, 1, 0);
+
+      // vec3 perturb = sin(pos * 10.);
+      // color = spectrum( dot(nor + perturb * .05, eyeDirection) * 2.);
+
+      // float specular = clamp(dot(reflection, lightDirection), 0., 1.);
+      // specular = pow((sin(specular * 20. - 3.) * .5 + .5) + .1, 32.) * specular;
+      // specular *= .1;
+      // specular += pow(clamp(dot(reflection, lightDirection), 0., 1.) + .3, 8.) * .1;
+
+      // float shadow = pow(clamp(dot(nor, dome) * .5 + 1.2, 0., 1.), 3.);
+      // color = color * shadow + (addSpecular ? specular : 0.0);
+
+      // float near = 2.8;
+      // float far = 8.;
+      // float fog = (collision.x - near) / (far - near);
+      // fog = clamp(fog, 0., 1.);
+      // color = mix(color, bg, fog);
+      // color = linearToScreen(color);
     }
 
     gl_FragColor = vec4(color, 1);
@@ -187,14 +218,26 @@ const sketch = async ({ gl, canvas }) => {
 
   const PARAMS = {
     camera: { x: 3.5, y: 0, z: 3.5 },
-    lensLength: 1.5,
+    light: { x: 1, y: 1, z: 1 },
+    lensLength: 2,
     spin: true,
+    tint: { r: 0.05, g: 0.0, b: 0.97 },
+    specular: false,
+    mix: 0.7,
+    animateTint: true,
   };
 
   const pane = new Pane();
   pane.addInput(PARAMS, 'camera', {});
+  pane.addInput(PARAMS, 'light', {});
   pane.addInput(PARAMS, 'lensLength', { min: 0, max: 5, step: 0.1 });
+  pane.addInput(PARAMS, 'mix', { min: 0, max: 1, step: 0.01 });
+  pane.addInput(PARAMS, 'tint', {
+    color: { type: 'float' },
+  });
   pane.addInput(PARAMS, 'spin', {});
+  pane.addInput(PARAMS, 'specular', {});
+  pane.addInput(PARAMS, 'animateTint', {});
 
   const mouse = createMouse(canvas);
 
@@ -203,8 +246,23 @@ const sketch = async ({ gl, canvas }) => {
     frag,
     uniforms: {
       cameraPos: () => Object.values(PARAMS.camera),
+      lightPos: () => Object.values(PARAMS.light),
       lensLength: () => PARAMS.lensLength,
+      addSpecular: () => PARAMS.specular,
       rotateCamera: () => PARAMS['spin'],
+      mixBaseAndIridescent: () => PARAMS.mix,
+      tint: ({ playhead }) =>
+        PARAMS.animateTint
+          ? lerpFrames(
+              [
+                [1, 0, 0],
+                [0, 1, 0],
+                [0, 0, 1],
+                [1, 0, 0],
+              ],
+              playhead
+            )
+          : Object.values(PARAMS.tint),
       resolution: ({ width, height }) => [width, height],
       time: ({ time }) => time,
       playhead: ({ playhead }) => playhead,
